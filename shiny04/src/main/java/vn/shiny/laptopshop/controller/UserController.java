6e8a98fc5422910bb9890fd189d1bbff4b394e1a
leqlong04 <leq.long2012@gmail.com>
2025-07-23 22:18:08 +0700

    view engine

--- shiny04/src/main/java/vn/shiny/laptopshop/controller/UserController.java
+++ shiny04/src/main/java/vn/shiny/laptopshop/controller/UserController.java
@@ -1,32 +1,46 @@
 package vn.shiny.laptopshop.controller;
 
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import vn.shiny.laptopshop.service.*;;
+import org.springframework.web.bind.annotation.RequestMapping;
+import vn.shiny.laptopshop.service.*;
 
 
-
-@RestController
+@Controller
 public class UserController {
-    
-    //DI
+
+        //DI
     private UserService UserService;
     
     public UserController(UserService userService) {
         UserService = userService;
     }
 
-    // @RequestMapping("/")    
-    // public String getHomePage() {
-    //     return "Hello from Controller";
-    // }
-    @GetMapping("")
+    @RequestMapping("/")
     public String getHomePage() {
-        return UserService.handleHello();
+        return "hello";
     }
     
-
 }
+
+// @RestController
+// public class UserController {
+    
+//     //DI
+//     private UserService UserService;
+    
+//     public UserController(UserService userService) {
+//         UserService = userService;
+//     }
+
+//     // @RequestMapping("/")    
+//     // public String getHomePage() {
+//     //     return "Hello from Controller";
+//     // }
+//     @GetMapping("")
+//     public String getHomePage() {
+//         return UserService.handleHello();
+//     }
+
+
+
