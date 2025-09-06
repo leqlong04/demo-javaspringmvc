9c9def1d3f94c886989a6c8ef4fc39410d7d892e
leqlong04 <leq.long2012@gmail.com>
2025-09-06 14:32:36 +0700

    update layout admin

--- shiny04/src/main/java/vn/shiny/laptopshop/config/WebMvcConfig.java
+++ shiny04/src/main/java/vn/shiny/laptopshop/config/WebMvcConfig.java
@@ -14,20 +14,22 @@ import org.springframework.web.servlet.view.JstlView;
 @EnableWebMvc
 public class WebMvcConfig implements WebMvcConfigurer {
     @Bean
-        public ViewResolver viewResolver() {
-            final InternalResourceViewResolver bean = new InternalResourceViewResolver();
-            bean.setViewClass(JstlView.class);
-            bean.setPrefix("/WEB-INF/view/");
-            bean.setSuffix(".jsp");
-            return bean;
-        }
+    public ViewResolver viewResolver() {
+        final InternalResourceViewResolver bean = new InternalResourceViewResolver();
+        bean.setViewClass(JstlView.class);
+        bean.setPrefix("/WEB-INF/view/");
+        bean.setSuffix(".jsp");
+        return bean;
+    }
+
     @Override
-        public void configureViewResolvers(ViewResolverRegistry registry) {
-            registry.viewResolver(viewResolver());
+    public void configureViewResolvers(ViewResolverRegistry registry) {
+        registry.viewResolver(viewResolver());
     }
 
     @Override
     public void addResourceHandlers(ResourceHandlerRegistry registry) {
         registry.addResourceHandler("/css/**").addResourceLocations("/resources/css/");
+        registry.addResourceHandler("/js/**").addResourceLocations("/resources/js/");
     }
 }
